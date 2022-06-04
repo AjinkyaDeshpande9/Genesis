ab5f058457d662eb70c70aa222d2505e04cbc25c
User <User@DESKTOP-1BVA85T>
2022-06-04 23:19:12 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Genesis
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.VerifyLable = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // VerifyLable
+            // 
+            this.VerifyLable.AutoSize = true;
+            this.VerifyLable.BackColor = System.Drawing.Color.Transparent;
+            this.VerifyLable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VerifyLable.Font = new System.Drawing.Font("Centauri", 20F);
+            this.VerifyLable.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(115)))), ((int)(((byte)(252)))));
+            this.VerifyLable.Location = new System.Drawing.Point(316, 281);
+            this.VerifyLable.Name = "VerifyLable";
+            this.VerifyLable.Size = new System.Drawing.Size(711, 29);
+            this.VerifyLable.TabIndex = 0;
+            this.VerifyLable.Text = "Please Verify Yourself";
+            this.VerifyLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+           
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Genesis.Properties.Resources.Textbox;
+            this.pictureBox1.Location = new System.Drawing.Point(321, 346);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(706, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Centauri", 32F);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(336, 362);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(673, 50);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.VerifyLable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label VerifyLable;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Genesis
+{
+    public partial class Login : Form
+    {
+        private string text;
+        private int len = 0;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+       private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (len < text.Length)
+            {
+                VerifyLable.Text = VerifyLable.Text + text.ElementAt(len);
+                len++;
+            }
+            
+            if (len > 0)
+            {
+                using (System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"D:\Projects\Genesis\Resources\Sound_LableLoad.wav"))
+                {
+                    player.Play();
+                    
+                }
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            text = VerifyLable.Text;
+            VerifyLable.Text = "";
+            timer1.Start();
+           
+        }
+
+      
+    }
+}
